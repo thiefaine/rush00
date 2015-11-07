af867186e81f0d3bf9eac573614955f5f36496ea
Matthias DUBRAY <mdubray@e1r3p5.42.fr>
2015-11-07 15:37:57 +0100

    maj

---
+++ Assets/character.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class character : MonoBehaviour {
+
+	public GameObject	head;
+	public GameObject	body;
+	public GameObject	foot;
+	//public Weapon		currentWeapon;
+
+	public Animator			animLegs;
+	protected Rigidbody2D	rb;
+
+	public float maxSpeed = 2.0f;
+	public float moveX = 0f;
+	public float moveY = 0f;
+
+	// Use this for initialization
+	void Start () {
+		animLegs  = this.foot.GetComponent<Animator>();
+		rb = this.GetComponent<Rigidbody2D>();
+	}
+
+
+	void FixedUpdate()
+	{
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
---
+++ Assets/hero.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class hero : character {
+
+	public Camera	cam;
+
+	// Use this for initialization
+	void Start () {
+		maxSpeed = 5.0f;
+	}
+
+	void FixedUpdate()
+	{	
+		moveX = Input.GetAxis("Horizontal"); 
+		moveY = Input.GetAxis("Vertical"); 
+		rb.velocity = new Vector2(moveX * maxSpeed, moveY * maxSpeed);
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+
+		head.transform.position = transform.position;
+		body.transform.position = transform.position;
+		foot.transform.position = transform.position;
+		cam.transform.position = new Vector3(transform.position.x, transform.position.y, cam.transform.position.z);
+	}
+}
